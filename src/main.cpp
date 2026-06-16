04ea88a9eef91948fdef3c263ccde0e256798743
Tobias Krč <supinacgaming@gmail.com>
2026-06-16 22:47:38 +0200

    fix for the 0.0.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
 #include <QWidget>
+#include <qboxlayout.h>
+#include <qcontainerfwd.h>
+#include <qobject.h>
+#include <qpushbutton.h>
+#include <qsizepolicy.h>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.setWindowTitle("Hello");
-    window.resize(400, 300);
-    window.show();
-    return app.exec();
+  QApplication app(argc, argv);
+
+  QWidget window;
+  window.setWindowTitle("Hello");
+
+  auto *layout = new QVBoxLayout(&window);
+  auto *label = new QLabel("clicks: 0");
+  auto *button = new QPushButton("push me");
+  layout->addWidget(button);
+  layout->addWidget(label);
+
+  int count = 0;
+  QObject::connect(button, &QPushButton::clicked, [&count, label]() {
+    label->setText(QString("clicks: %1").arg(++count));
+  });
+
+  window.resize(400, 300);
+  window.show();
+  return app.exec();
 }
